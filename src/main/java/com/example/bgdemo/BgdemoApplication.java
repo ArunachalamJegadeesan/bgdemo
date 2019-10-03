17c8252061e89b42cab567c91fe5f28455287c20
ArunachalamJegadeesan <inboxofarun@gmail.com>
2019-10-03 07:40:06 +0530

    second

---
+++ src/main/java/com/example/bgdemo/BgdemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.bgdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BgdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgdemoApplication.class, args);
+	}
+
+	@GetMapping("/home")
+	public String home(){
+		return "BLUE";
+	}
+
+}
---
+++ src/test/java/com/example/bgdemo/BgdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.bgdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BgdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
